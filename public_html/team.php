4d3f3afcebbd3a48b453d8a0375e4b0c019e913b
Michael Fritz <mf35luzo@studserv.uni-leipzig.de>
2017-11-11 11:50:20 +0100

    added Sus to Website

--- public_html/team.php
+++ public_html/team.php
@@ -9,7 +9,7 @@ include 'header.php';
      document.getElementById("nav_projekt").style.backgroundColor = '#D8D8D8';
     </script>
     
-        <article class="article">
+        <article class="article" id="team">
             <h2>Unser Team</h2>
             <table>
             <tr>
@@ -17,11 +17,11 @@ include 'header.php';
                 <th>Rolle</th>
             </tr>
             <tr>
-                <td>Felix Fink</td>
+                <td><a href="#FF">Felix Fink</a></td>
                 <td></td>
             </tr>
              <tr>
-                <td>Christian Schlecht</td>
+                <td><a href="#CS">Christian Schlecht</a></td>
                 <td>(vorerst) Projektleiter</td>
             </tr>
              <tr>
@@ -37,11 +37,11 @@ include 'header.php';
                 <td></td>
             </tr>
              <tr>
-                <td>Thomas Gerbert</td>
+                <td><a href="#TG">Thomas Gerbert</a></td>
                 <td></td>
             </tr>
              <tr>
-                <td>Michael Fritz</td>
+                <td><a href="#MF">Michael Fritz</a></td>
                 <td>(vorerst) Webmaster</td>
             </tr>
              <tr>
@@ -52,6 +52,130 @@ include 'header.php';
             
 
         </article>
+    
+         <article class="article" id="sus">
+              <h2>St&auml;rken und Schw&auml;chen</h2>
+             
+             
+             <h3 id="FF">Felix Fink</h3>
+             <p>
+            <b>Programmieren:</b><br/>
+
+    + Java                       <br/>
+    + geringe Kenntnisse mit Android Studio    <br/>
+    - nur sehr geringf&uuml;gig au&szlig;erhalb des OOP programmiert    <br/>
+<br/>
+<b>Organisatorisch:</b>   <br/>
+
+    - ben&ouml;tige feste Planung vor Abgaben  <br/>
+    <br/>
+<b>Kreativ:</b>  <br/>
+
+    + Ideenreich (wenn nicht unter Stress) <br/>
+    + Interesse an Designentwicklung  <br/>
+    <br/>
+<b>Gamifikation:</b>    <br/>
+
+    + Interresse an der Psychologie <br/>
+    - noch keine Erfahrung im Bezug auf die betriebliche Weiterbildung    <br/>
+    <br/>
+<b>Pers&ouml;nlich:</b>        <br/>
+
+    + Kommunikationsf&auml;hig   <br/>
+    + Teamf&auml;hig
+             
+             
+             </p>
+             
+             
+             <h3 id="CS">Christian Schlecht</h3>
+            <p>
+            <b>Programmieren:</b><br/>
+    + Java (vor allem im Rahmen OOP)<br/>
+    + Basics in Python, JavaScript     <br/>
+    + Arbeit mit Libraries (z.B. mit p5.js gearbeitet)   <br/>
+    - Rekursionen <br/>
+    - Backend Kenntnisse  <br/>
+            <br/>
+<b>Organisatorisch:</b>    <br/>
+    + Arbeit/Kommunikation im Team  <br/>
+    + p&uuml;nktlich bzw. zuverl&auml;ssig <br/>
+    - manchmal chaotisch (alles am besten sofort anfangen, nicht erst eins fertig und dann das n&auml;chste) <br/>
+       <br/>
+<b>Sonstiges:</b>     <br/>
+    - ungeduldig (vor allem wenn etwas nicht funktioniert)  <br/>
+    - "from scratch" Implementieren, Konzepte entwickeln 
+
+            
+            </p>
+             
+             
+             <h3 id="TG">Thomas Gerbert</h3>
+             <p>
+                <b>Programmieren:</b><br/>
+    + Java                           <br/>
+    + Python, C Basics                    <br/>
+    - keine Erfahrung mit gr&ouml;&szlig;eren Projekten   <br/>
+                                                    <br/>
+<b>Organisatorisch</b>:                                        <br/>
+    + p&uuml;nktlich                                               <br/>
+    - &Uuml;bernimmt ungerne gro&szlig;e Verantwortung                        <br/>
+    - kann ohne Deadline nicht arbeiten                                 <br/>
+                                                                             <br/>
+<b>Kreativ:</b>                                                                         <br/>
+    + Ideen Einbringung                                                                <br/>
+                                                                                            <br/>
+
+<b>Gamification:</b>                                                                                   <br/>
+    + Interesse an Psychologischen Themen                                                             <br/>
+    + Selber "Spieler/Gamer"                                                                               <br/>
+    - Eher neu in dem Thema                                                                                    
+
+             
+             
+             
+             </p>
+             
+             
+             
+              <h3 id="MF">Michael Fritz</h3>
+              <p>
+              <b>Programmieren:</b>  <br/>
+                + Java (auch Android) <br/>
+                + C , C++  <br/>
+                + PHP , HTML , CSS , SQL , JavaScript <br/>
+                + Arbeiten mit Datenbanken <br/>
+                - in manchen Sachen sehr festgefahren --> bennenung von Variablen etc. (richtung Ungarische-Notation)     <br/>
+                <br/>
+            <b>Struckturell:</b> <br/>
+                + Struckturierte UML etc. in Quelltext umwandeln <br/> 
+                - Entwickeln von Konzepten <br/>
+                - Eher drauf los programmieren, experementiell <br/>
+              <br/>
+            <b>Organisatorisch: </b><br/>
+                + p&uuml;nktlich    <br/>
+                + Struckturierte Darstellungen f&uuml;r Informationen <br/>
+                + Sehr Motiviert wenn mich ein Thema Interessiert / Spa&szlig; macht (beim Programmieren fast immer)<br/>
+                - Vermeidet zusatzarbeit bei uninteressanten Themen  <br/>
+                - Ideen Durchsetzten <br/>     
+                - &Uuml;bernimmt ungerne gro&szlig;e Verantwortung <br/>
+                - Wenn etwas nicht klappt schnell ungeduldig und festgebissen <br/>
+                <br/>
+            <b>Kreativ:</b> <br/>
+                 Entwickeln von Design      <br/>
+                 Erstellen von Ressourcen  <br/>
+                - Responsive Designs <br/>
+                <br/>
+        <b>Gamification:</b> <br/>
+            + Interesse an Psychologischen Themen  <br/>
+            + Selber "Spieler/Gamer"  <br/>
+            - Eher neu in dem Thema   <br/>
+          <br/>
+        <b>Sonstiges:</b>   <br/>
+            +Lernt schnell neue Sachen
+              </p>
+              
+         </article>
     
     
     
