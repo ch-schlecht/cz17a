46d3f3bd490a824cec893c4654042ac3f602af46
WillySteinbach <ws53nawe@studserv.uni-leipzig.de>
2018-01-17 19:03:08 +0100

    dynamische Buttons

--- Code/Client/app/src/main/java/com/example/cz17a/quizclient/TopicSelection.java
+++ Code/Client/app/src/main/java/com/example/cz17a/quizclient/TopicSelection.java
@@ -6,8 +6,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class TopicSelection extends AppCompatActivity {
 
@@ -18,17 +20,24 @@ public class TopicSelection extends AppCompatActivity {
         final Button topic1 = findViewById(R.id.butTopic1);
         final TopicHandler topicHandler = new TopicHandler(topic1);
         topicHandler.setUpActivity();
+        addButton();
         topic1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 goToGame();
             }
         });
-
     }
     public void goToGame(){
         Intent intent = new Intent(this,  GameActivity.class);
         startActivity(intent);
+    }
+
+    public void addButton(){
+        System.out.println("Neuer Button");
+        Button b = new Button(this);
+        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
+        linLayout.addView(b);
     }
 
 
