4cf17436ef61f881092753686f0e222bb4ba74e4
Valekta <karuri@web.de>
2018-01-23 12:57:55 +0100

    Add rest method for getting answer to a question by id

--- Code/Server/restserver/src/main/java/cz17a/gamification/restserver/resource/quiz/AnswerResource.java
+++ Code/Server/restserver/src/main/java/cz17a/gamification/restserver/resource/quiz/AnswerResource.java
@@ -6,9 +6,12 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import data.access.AnswerDAO;
+import data.access.QuestionDAO;
+import data.access.QuizDAO;
 import data.model.Answer;
 import data.model.Question;
 
@@ -23,6 +26,26 @@ public class AnswerResource {
 		AnswerDAO dao = new AnswerDAO();
 		List<Answer> answers = new ArrayList<Answer>();
 		for(Answer a : dao.getAnswers()) {
+			Question question = new Question();
+			question.setDynamic_difficulty(a.getQuestion().getDynamic_difficulty());
+			question.setId(a.getQuestion().getId());
+			question.setQuestioning(a.getQuestion().getQuestioning());
+			question.setResponse_time(a.getQuestion().getResponse_time());
+			question.setStatic_difficulty(a.getQuestion().getStatic_difficulty());
+			question.setTopic(a.getQuestion().getTopic());
+			a.setQuestion(question);
+			answers.add(a);
+		}
+		return answers;
+	}
+	
+	@GET
+	@Path("/questions/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Answer> getAnswersByQuestionId(@PathParam("id") int question_id){
+		QuestionDAO dao = new QuestionDAO();
+		List<Answer> answers = new ArrayList<Answer>();
+		for(Answer a : dao.getQuestion(question_id).getAnswers()) {
 			Question question = new Question();
 			question.setDynamic_difficulty(a.getQuestion().getDynamic_difficulty());
 			question.setId(a.getQuestion().getId());
