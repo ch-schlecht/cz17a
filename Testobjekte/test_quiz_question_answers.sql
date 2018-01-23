93dd7a1edfd625609b5072c2ef217618b7800db1
Valekta <karuri@web.de>
2018-01-23 17:00:24 +0100

    Add sql script to put some test answers in db with linked question and quiz

---
+++ Testobjekte/test_quiz_question_answers.sql
@@ -0,0 +1,24 @@
+﻿insert into Quiz (id, title, length, min_participants, max_participants)
+values ('1','test','10','1','5');
+
+insert into Quiz (id, title, length, min_participants, max_participants)
+values ('2','test2','10','1','5');
+
+insert into Question(id, response_time, questioning, dynamic_difficulty, static_difficulty, topic, quiz_id)
+values ('1','60','Wer bich ich?','1','1','testen','1');
+
+insert into Question(id, response_time, questioning, dynamic_difficulty, static_difficulty, topic, quiz_id)
+values ('2','60','Was bich ich?','1','1','testen2',2);
+
+insert into Answer (question_id, id, content, type)
+values ('1','1','Der Nikolaus','true');
+
+insert into Answer (question_id, id, content, type)
+values ('1','2','Der Weinachtsmann','false');
+
+insert into Answer (question_id, id, content, type)
+values ('2','3','Ein Auto','true');
+
+insert into Answer (question_id, id, content, type)
+values ('2','4','Ein Kreisel','false');
+
