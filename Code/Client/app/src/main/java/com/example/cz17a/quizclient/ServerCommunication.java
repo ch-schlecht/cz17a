6857ceada814b55fdd0c1a5fb02c98ce7d440772
Thomas Gerbert <tg55jyka@studserv.uni-leipzig.de>
2018-01-27 18:10:54 +0100

    solved mergeconflikt

--- Code/Client/app/src/main/java/com/example/cz17a/quizclient/ServerCommunication.java
+++ Code/Client/app/src/main/java/com/example/cz17a/quizclient/ServerCommunication.java
@@ -1,5 +1,7 @@
 package com.example.cz17a.quizclient;
 
+import android.widget.ArrayAdapter;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -8,8 +10,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by stein on 17.01.2018.
@@ -18,122 +22,55 @@ import java.net.URL;
 
 public class ServerCommunication {
 
-        private String url_root = null;
-        private String url_quiz = null;
-        private String url_quest = null;
-        private String url_answer = null;
-        private String nudes = null;
+       final private String url_root = "http://pcai042.informatik.uni-leipzig.de:1810/restserver/";
+       final private String url_quiz = null;
+       final private String url_quest = null;
+       final private String url_answer = null;
+       private String nudes = null;
 
 
-    public JSONObject getQuizType() {
-
-        String quizType = null;
-        try {
-            if(url_quiz != null && url_root != null) {
-                URL url = new URL(url_root + url_quiz);
-                HttpURLConnection connect = (HttpURLConnection) url.openConnection();
-                connect.setRequestMethod("GET"); //throws ProtocolException
-                connect.connect();
-
-                if (connect.getResponseCode() != 200) {
-                    throw new RuntimeException("Failed : HTTP error code: " + connect.getResponseCode());
-                }
-
-                BufferedReader cIn = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-                while (cIn.readLine() != null) {
-                    quizType += cIn;
-                }
-
-                connect.disconnect();
-
-            }else{
-                System.out.println("URL nicht gesetzt");
-                return null;
-            }
-        } catch (IOException e) {
-
-        }
+    public JSONObject getQuizTypeJSON() {
         JSONObject jType = null;
-        try {
-            jType = new JSONObject(quizType);
-        } catch (JSONException j) {
-            System.out.println("invalid Data");
-        }
-        return jType;
+      try {
+          URL url = new URL(url_root + url_quiz);
+          jType = ask(url);
+      }catch(IOException e){
+
+      }
+
+      return null;
     }
 
-    public JSONObject getQuestionsJSON() {
-        String quest = null;
-        try {
-            if(url_quest != null && url_root != null) {
-                URL url = new URL(url_root + url_quest);
-                HttpURLConnection connect = (HttpURLConnection) url.openConnection();
-                connect.setRequestMethod("GET"); //throws ProtocolException
-                connect.connect();
 
-                if (connect.getResponseCode() != 200) {
-                    throw new RuntimeException("Failed : HTTP error code: " + connect.getResponseCode());
-                }
 
-                BufferedReader cIn = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-                while (cIn.readLine() != null) {
-                    quest += cIn;
-                }
-
-                connect.disconnect();
-
-            }else{
-                System.out.println("URL nicht gesetzt");
-                return null;
-            }
-        } catch (IOException e) {
-
-        }
+    public JSONObject getQuestionsJSON(int id) {
         JSONObject jType = null;
         try {
-            jType = new JSONObject(quest);
-        } catch (JSONException j) {
-            System.out.println("invalid Data");
+            URL url = new URL(url_root + url_quest + "/" + id);
+            jType = ask(url);
+        }catch(IOException e){
+
         }
-        return jType;
+
+        return null;
     }
 
-    public JSONObject getAnswersJSON() {
-        String answer = null;
-        try {
-            if(url_quest != null && url_root != null) {
-                URL url = new URL(url_root + url_answer);
-                HttpURLConnection connect = (HttpURLConnection) url.openConnection();
-                connect.setRequestMethod("GET"); //throws ProtocolException
-                connect.connect();
 
-                if (connect.getResponseCode() != 200) {
-                    throw new RuntimeException("Failed : HTTP error code: " + connect.getResponseCode());
-                }
 
-                BufferedReader cIn = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-                while (cIn.readLine() != null) {
-                    answer += cIn;
-                }
-
-                connect.disconnect();
-
-            }else{
-                System.out.println("URL nicht gesetzt");
-                return null;
-            }
-        } catch (IOException e) {
-
-        }
+    public JSONObject getAnswersJSON(int id) {
         JSONObject jType = null;
         try {
-            jType = new JSONObject(answer);
-        } catch (JSONException j) {
-            System.out.println("invalid Data");
+            URL url = new URL(url_root + url_answer + "/" + id);
+            jType = ask(url);
+        }catch(IOException e){
+
         }
-        return jType;
+
+        return null;
     }
 
+
+    // under construction
     public void sendNudes(){
         try{
             if(url_root != null && nudes != null){
@@ -153,38 +90,33 @@ public class ServerCommunication {
     }
 
 
-    //Fügt Fragen zum Fragenkatalog hinzu, wandelt JSON in Frage
-    public Question[] getQuestions(){
-        //dummy für Fragenanzahl, Anzahl soll aus JsonObj erzeugt werden
-        int questionCount = 3;
-        //Array für die Frage
-        Question[] questionArray = new Question[questionCount];
 
-        //Dummy um die Fragen zu erzeugen -> Replace with JSON import
-        //setFrageText und SetQuestionID
-        for (int i = 0; i<questionCount; i++){
-            questionArray[i] = new Question();
-            questionArray[i].setQuestionText("Test Frage " + i);
-            questionArray[i].setQuestionID(i);
-        }
-        getAnswers(questionArray);
-        return questionArray;
-    }
-    public void getAnswers(Question[] questionArray){
-        for(int i= 0;i<questionArray.length; i++){
-            //ID Der Frage mit questionArray[i].getID();
-            //Damit dann JSONObj anfordern und die Antworten der Frage damit füllen
+    public JSONObject ask(URL url){
+        String quizType = null;
+        try {
+             HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+            connect.setRequestMethod("GET"); //throws ProtocolException
+            connect.connect();
 
-            //dummy -> Replace with JSON import
-            String[] answers = new String[4];
-            for(int f = 0; f<4; f++){
-                answers[f] = "TestAntwort " + f;
+            if (connect.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code: " + connect.getResponseCode());
             }
-
-            //Setzen der Antworten für die Frage
-            questionArray[i].setAnswers(answers);
-
+            BufferedReader cIn = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+            while (cIn.readLine() != null) {
+                quizType += cIn;
+            }
+            connect.disconnect();
+        } catch (IOException e) {
+            System.out.println("cant connect to given URL");
+            return null;
         }
+        JSONObject jType = null;
+        try {
+            jType = new JSONObject(quizType);
+        } catch (JSONException j) {
+            System.out.println("invalid Data");
+        }
+        return jType;
     }
 
 
