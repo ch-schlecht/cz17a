ec5a6210c4a053c77cd84bee653385df97093624
Valekta <karuri@web.de>
2018-02-18 13:01:08 +0100

    Add annotation in User for generated id by database

--- Code/Server/restserver/src/main/java/data/model/User.java
+++ Code/Server/restserver/src/main/java/data/model/User.java
@@ -4,6 +4,8 @@ import java.util.Calendar;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
@@ -12,6 +14,7 @@ import javax.persistence.InheritanceType;
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class User {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	protected int id;
 	protected String mail;
 	protected String nickname;
