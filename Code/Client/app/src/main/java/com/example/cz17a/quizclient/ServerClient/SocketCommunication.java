5af3793daa82acaad0aee08a1dc0a355cc57cd95
Felix Fink <ff14hifu@studserv.uni-leipzig.de>
2018-03-05 21:33:31 +0100

    Dummy Socketcommunication

--- Code/Client/app/src/main/java/com/example/cz17a/quizclient/ServerClient/SocketCommunication.java
+++ Code/Client/app/src/main/java/com/example/cz17a/quizclient/ServerClient/SocketCommunication.java
@@ -18,28 +18,35 @@ import java.net.UnknownHostException;
  */
 
 public class SocketCommunication implements Runnable{
-// willy fk y
-    ServerSocket socket = null;
+
+    //ServerSocket socket = null;
     Socket server = null;
+
     BufferedReader in = null;
     char buffer[] = new char[1024];
     PrintWriter out = null;
     int port = 0;
     boolean running;
+    String[] statusMessages;
+    int playernumberGameStartedWith;
+    String gameID;
 
     public SocketCommunication(int port){
-        /*
+
         this.port = port;
+
         running = true;
-        connect();
+        connect();/*
         */
     }
 
+    /**
+     * Establish a connection to the Server Socket and initialize Input- and OutputStreams.
+     */
    public void connect() {
-
        try {
-           socket = new ServerSocket(port);
-           server = socket.accept(); //wait for connection from server (in this case client)
+           //socket = new ServerSocket(port);
+           server = new Socket("IP", port); //socket.accept(); //wait for connection from server (in this case client)
            in = new BufferedReader(new InputStreamReader(server.getInputStream()));
            out = new PrintWriter(server.getOutputStream(), true);
 
@@ -53,11 +60,73 @@ public class SocketCommunication implements Runnable{
        }
    }
 
-   public void disconnect() throws IOException {
+    /**
+     * Method that receives all Messages from Server Socket and saves them in the String Array.
+     */
+   public void receiveMessagesFromServer(){
+        int i=0;
+        while (true){
+            try {
+                System.out.println(in.readLine());
+                if (in.readLine() == null){
+                    continue;
+                }
+                else {
+                    statusMessages[i] = in.readLine();
+                    i++;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+   }
+
+    /**
+     * Tests if there are enough Player for a Game.
+     *
+     * @return boolean if there are enough People to Play a Round in this Lobby.
+     */
+   public boolean enoughPlayerToPlay(int minNumberofPlayer){
+
+           int numberOfPeopleInLobby = 0;
+           for(String message: statusMessages){
+               if (message.startsWith("player:") && message != null){
+                   numberOfPeopleInLobby++;
+                   if (numberOfPeopleInLobby >= minNumberofPlayer){
+                       playernumberGameStartedWith = numberOfPeopleInLobby;
+                       return true;
+
+                   }else continue;
+
+               } else if (message == null){
+                   return false;
+               } else continue;
+           }
+           return false;
+
+   }
+
+    /**
+     * Gets the gameID out of the status messages.
+     */
+   void gameID(){
+       gameID = statusMessages[playernumberGameStartedWith-1];
+   }
+
+    /**
+     *
+     * @return gameID from the started Game.
+     */
+   public String getGameID(){
+       return gameID;
+   }
+
+  /* public void disconnect() throws IOException {
        if(socket != null && !socket.isClosed()){
            socket.close();
        }
-   }
+   }*/
 
 
    public void sendAnswer(String answer){
