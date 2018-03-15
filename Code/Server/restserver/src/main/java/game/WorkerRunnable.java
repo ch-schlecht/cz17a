936772a889bc27d4ba4ab7b0cac6124e8da277fa
DESKTOP-VG4VM5S\KaTo <karuri@web.de>
2018-03-15 20:50:30 +0100

    Format code in Worker Runnable

--- Code/Server/restserver/src/main/java/game/WorkerRunnable.java
+++ Code/Server/restserver/src/main/java/game/WorkerRunnable.java
@@ -9,70 +9,57 @@ import data.model.Player;
 
 /**
  * Runnable for ThreadPool
+ * 
  * @author Michael
  * @version 1.0
  *
  */
-public class WorkerRunnable implements Runnable{
+public class WorkerRunnable implements Runnable {
 
 	protected Socket clientSocket = null;
-    protected ServerThreadPool parent   = null;
-    protected int ID = 0;
-    
-    public WorkerRunnable(Socket clientSocket, ServerThreadPool parent, int ID) {
-        this.clientSocket = clientSocket;
-        this.parent = parent;
-        this.ID = ID;
-    }
+	protected ServerThreadPool parent = null;
+	protected int ID = 0;
 
+	public WorkerRunnable(Socket clientSocket, ServerThreadPool parent, int ID) {
+		this.clientSocket = clientSocket;
+		this.parent = parent;
+		this.ID = ID;
+	}
 
-    public void run() {
-       System.out.println("running Pool");
-    	while(true){
-    		OutputStream output = null;
-            try {
-            	
-            	String msg = parent.message.get(ID);
-            	if(msg != "") {
-            		output = clientSocket.getOutputStream();
-    				output.write(msg.getBytes());
-    				output.flush();
-    				System.out.println("Sending "+msg+" to "+clientSocket.getInetAddress());
-    				parent.message.set(ID, "");
-            	}
-            	
-            	
-            } catch (IOException e) {
+	public void run() {
+		System.out.println("running Pool");
+		while (true) {
+			OutputStream output = null;
+			try {
+				String msg = parent.message.get(ID);
+				if (msg != "") {
+					output = clientSocket.getOutputStream();
+					output.write(msg.getBytes());
+					output.flush();
+					System.out.println("Sending " + msg + " to " + clientSocket.getInetAddress());
+					parent.message.set(ID, "");
+				}
+
+			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-            
-    		
-    		/**try {
-            
-        		if(parent.message != null) {
-            	   
-               }
-        		
-        		
-        		
-        		InputStream input  = clientSocket.getInputStream();
-                OutputStream output = clientSocket.getOutputStream();
-                long time = System.currentTimeMillis();
-                output.write(("HTTP/1.1 200 OK\n\nWorkerRunnable: " +
-                		this.serverText + " - " +
-                		time +
-                		"").getBytes());
-                output.close();
-                input.close();
-                System.out.println("Request processed: " + time);
-            } catch (IOException e) {
-                //report exception somewhere.
-                e.printStackTrace();
-            }**/
-        }
-
-        	
-    }
-    
-    }
+			/**
+			 * try {
+			 * 
+			 * if(parent.message != null) {
+			 * 
+			 * }
+			 * 
+			 * 
+			 * 
+			 * InputStream input = clientSocket.getInputStream(); OutputStream output =
+			 * clientSocket.getOutputStream(); long time = System.currentTimeMillis();
+			 * output.write(("HTTP/1.1 200 OK\n\nWorkerRunnable: " + this.serverText + " - "
+			 * + time + "").getBytes()); output.close(); input.close();
+			 * System.out.println("Request processed: " + time); } catch (IOException e) {
+			 * //report exception somewhere. e.printStackTrace(); }
+			 **/
+		}
+	}
+}
